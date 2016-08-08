4e223282327e6d9811257ee541f3e9f4daf946bc
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-08-08 14:45:08 -0500

    Day 1 lab

--- Day1/lab1.py
+++ Day1/lab1.py
@@ -1,34 +1,82 @@
-def binarify(num): 
-  """convert positive integer to base 2"""
-  if num<=0: return '0'
-  digits = []
-  return ''.join(digits)
+import math
 
+def binarify(num):
+  	if num<=0: return '0'
+	digits = []
+	cur = num
+	while(cur>0):
+		digits.append(str(cur%2))
+		cur/=2
+	digits.reverse()
+
+	return ''.join(digits)
+		
+	
 def int_to_base(num, base):
   """convert positive integer to a string in any base"""
-  if num<=0:  return '0' 
+  if base<=0:  return '0' 
   digits = []
+  if (num <0):
+  	cur= -num
+  else: cur = num
+  while(cur>0):
+		digits.append(str(cur%base))
+		cur/=base
+  if (num <0): digits.append('-')
+  digits.reverse()
+
+  
+  
   return ''.join(digits)
 
 def base_to_int(string, base):
   """take a string-formatted number and its base and return the base-10 integer"""
   if string=="0" or base <= 0 : return 0 
   result = 0 
+  flip = False;
+  if string[0]=='-':
+  	flip=True;
+  	string = string[1:]
+  pow = len(string)-1
+  for letr in string:
+  	letrNum = int(letr)
+  	result+= letrNum*(base**pow)
+  	pow-=1
+  if flip:
+  	result= -result
   return result 
 
 def flexibase_add(str1, str2, base1, base2):
   """add two numbers of different bases and return the sum"""
-  result = int_to_base(tmp, base1)
-  return result 
+  n1 = base_to_int(str1, base1)
+  n2 = base_to_int(str2, base2)
+  #result = int_to_base(tmp, base1)
+  return n1+n2
 
 def flexibase_multiply(str1, str2, base1, base2):
   """multiply two numbers of different bases and return the product"""
-  result = int_to_base(tmp, base1)
-  return result 
+  n1 = base_to_int(str1, base1)
+  n2 = base_to_int(str2, base2)  
+  return n1*n2 
 
 def romanify(num):
   """given an integer, return the Roman numeral version"""
   result = ""
+  onesDict = {1:"I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 0:""}
+  ones = num%10
+  num-=num%10
+  result = onesDict[ones] + result
+  tensDict = {10:"X", 20: "XX", 30: "XXX", 40:"XL", 50:"L", 60:"LX", 70: "LXX", 80: "LXXX", 90: "XC", 0:""}
+  tens = num%100
+  num-=num%100
+  result = tensDict[tens] + result
+  hunsDict = {100:"C", 200: "CC", 300: "CCC", 400:"CD", 500:"D", 600:"DC", 700: "DCC", 800: "DCCC", 900: "CM", 0:""}
+  huns = num%1000
+  num-=num%1000
+  result = hunsDict[huns] + result
+  thous = num/1000
+  result = "M"*thous + result
+  
   return result
   
 # Copyright (c) 2014 Matt Dickenson
