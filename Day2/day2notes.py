3ab0021ae5372614db14918d2c85a8dfca7367f1
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-08-10 12:15:28 -0500

    idk

--- Day2/day2notes.py
+++ Day2/day2notes.py
@@ -7,3 +7,6 @@ print werd
 r1 = uniform(20,30)
 
 print r1
+
+##NOTE: difference between class attribute and instance attribute
+## one instance of 'human' class can have an instance attribute of 'latin name'
