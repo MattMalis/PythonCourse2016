da77e75e58c2bd6b210ae931c94f71ef992568cb
MattMalis <iramalis@nah.local>
2016-08-14 20:51:40 -0500

    modified stock and mutual fund with polymorphism of Account class

--- Homework/HW1/portfolio.py
+++ Homework/HW1/portfolio.py
@@ -19,70 +19,81 @@ class CashAccount():
 	
 	def __str__(self):
 		return "Cash account for portfolio named %s contains: \n\t$%s in cash\n" %(self.portfolio, self.amount)
+
+class Asset():
+	def __init__(self, share_price, symbol):
+		self.symbol = symbol
+		self.share_price = share_price
 		
-class StockAccount():
-	def __init__(self, portfolio):
-		self.stock_list = {}
+class AssetAccount():
+	def __init__(self, portfolio, asset_type=None):
+		self.asset_list = {}
 		self.portfolio = portfolio
+		self.asset_type = asset_type
+	
+	def addAsset(self, shares, asset):
+		if shares<0:
+			return self.removeStock(-shares, asset)
+		if asset in self.asset_list:
+			self.asset_list[asset]+=shares
+		else: self.asset_list[asset] = shares
+	def removeAsset(self, shares, asset):
+		if shares<0:
+			return self.addStock(-shares, asset)
+		if not asset in self.asset_list.keys():
+			raise CustomException("cannot sell shares of %s, because you currently do not own any" %(asset.symbol))
+		if self.asset_list[asset]<shares:
+			raise CustomException("cannot sell more shares of a %s than currently in portfolio" %(self.asset_type))
+		self.asset_list[asset] -= shares
+	
+	def __str__(self):
+		asset_report = ""
+		for a in self.asset_list:
+			asset_report += '\t' + str(self.asset_list[a]) + " shares of " + str(a.symbol) + " priced at $" + str(a.share_price) + ' each \n'
+		return "%s account for portfolio named %s contains: \n %s" %(self.asset_type, self.portfolio, asset_report)
+
+class StockAccount(AssetAccount):
+	def __init__(self, portfolio):
+		AssetAccount.__init__(self, portfolio, "stock")
+		
 	
 	def addStock(self, shares, stock):
 		if not isinstance(stock, Stock):
 			raise CustomException("attempt to add object to stock account that was not an instance of class Stock")
 		if not isinstance(shares, int):
 			raise CustomException("can only buy/sell stocks by whole shares (integer values)")
-		if shares<0:
-			return self.removeStock(-shares, stock)
-		if stock in self.stock_list:
-			self.stock_list[stock]+=shares
-		else: self.stock_list[stock] = shares
+		return self.addAsset(shares, stock)
+	
 	def removeStock(self, shares, stock):
+		if not isinstance(stock, Stock):
+			raise CustomException("attempt to remove object from stock account that was not an instance of class Stock")
 		if not isinstance(shares, int):
 			raise CustomException("can only buy/sell stocks by whole shares (integer values)")
-		if shares<0:
-			return self.addStock(-shares, stock)
-		if self.stock_list[stock]<shares:
-			raise CustomException("cannot sell more shares of a stock than currently in portfolio")
-		self.stock_list[stock] -= shares
-	
-	def __str__(self):
-		stock_report = ""
-		for s in self.stock_list:
-			stock_report += '\t' + str(self.stock_list[s]) + " shares of " + str(s.symbol) + " priced at $" + str(s.share_price) + ' each \n'
-		return "Stock account for portfolio named %s contains: \n %s" %(self.portfolio, stock_report)
-
-class Stock():
-	def __init__(self, share_price, symbol):
-		self.symbol = symbol
-		self.share_price = share_price
+		return self.removeAsset(shares, stock)
 		
-class MutualFundAccount():
+class Stock(Asset):
+	def __init__(self, share_price, symbol):
+		Asset.__init__(self, share_price, symbol)
+		
+class MutualFundAccount(AssetAccount):
 	def __init__(self, portfolio):
-		self.mf_list = {}	
-		self.portfolio = portfolio
+		AssetAccount.__init__(self, portfolio, "mutual fund")
+		
 	def addMF(self, shares, mf):
-		if shares<0:
-			return self.removeMF(-shares, mf)
-		if mf.symbol in self.mf_list:
-			self.mf_list[mf]+=shares
-		else: self.mf_list[mf] = shares
-	def removeMF(self, shares, mf):
-		if shares<0:
-			return self.addMF(-shares, mf)
-		if self.mf_list[mf]<shares:
-			raise CustomException("cannot sell more shares of a mutual fund than currently in portfolio")
-		self.mf_list[mf] -= shares
-	
-	def __str__(self):
-		mf_report = ""
-		for mf in self.mf_list:
-			mf_report += '\t' + str(self.mf_list[mf]) + " shares of " + str(mf.symbol) + " priced at $" + str(mf.share_price) + " each \n"
-		return "Mutual fund account for portfolio named %s contains:\n%s" %(self.portfolio, mf_report)
+		if not isinstance(mf, MutualFund):
+			raise CustomException("attempt to add object to mutual fund account that was not an instance of class MutualFund")
+		return self.addAsset(shares, mf)
 
+	def removeMF(self, shares, mf):
+		if not isinstance(mf, MutualFund):
+			raise CustomException("attempt to remove object from mutual fund account that was not an instance of class MutualFund")
+		return self.removeAsset(shares, mf)
 		
 class MutualFund():
 	def __init__(self, symbol, share_price=1):
 		self.symbol = symbol
 		self.share_price = share_price
+
 		
 class Portfolio():
 	def __init__(self, client=" "):
@@ -106,7 +117,7 @@ class Portfolio():
 		
 	def sellStock(self, shares, stock):
 		if not isinstance(stock, Stock): ## if arg is not of class Stock
-			for item in self.stock_account.stock_list.keys(): ## check to see if it is the stock symbol of one of the portfolio's stocks
+			for item in self.stock_account.asset_list.keys(): ## check to see if it is the stock symbol of one of the portfolio's stocks
 				if item.symbol == stock:
 					stock = item ## ...and swap the symbol fo for the stock object itself
 					break
@@ -123,7 +134,7 @@ class Portfolio():
 		
 	def sellMutualFund(self, shares, mf):
 		if not isinstance(mf, MutualFund):
-			for item in self.mutual_fund_account.mf_list.keys():
+			for item in self.mutual_fund_account.asset_list.keys():
 				if item.symbol == mf:
 					mf = item
 					break
@@ -154,13 +165,16 @@ try:
 	portfolio.sellStock(1,"HFH")
 	portfolio.withdrawCash(50)
 	portfolio.history()
-	portfolio.mutual_fund_account.mf_list[MutualFund("DNE")] = 30
+	portfolio.mutual_fund_account.asset_list[MutualFund("DNE")] = 30
 	#portfolio.sellMutualFund(45, "DNE")
 	portfolio.sellMutualFund(25, "DNE")
 	portfolio.buyStock(3, Stock(18.5, "MIM"))
-	print(portfolio)
+	#print(portfolio)
 	portfolio.withdrawCash(100)
 	portfolio.sellStock(3, "MIM")
+	ibm = Stock(76, "IBM")
+	#portfolio.sellStock(8, "IBM")
+	portfolio.sellStock(8, ibm)
 	portfolio.history()
 except CustomException as inst:
 	print("\nError: %s" %(inst.value))
