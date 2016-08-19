75db8753e9e26321f8fc60218119ec49360aec5d
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-08-19 11:40:26 -0500

    lab6 divisor and primes functions

---
+++ Day6/lab6_mm.py
@@ -0,0 +1,37 @@
+#Exercise 1
+#Write a function to calculate the greatest common divisor of two numbers
+
+def gcf(a, b, divisor=0): ## just don't provide an argument for divisor and this will work out ok...
+	if (a==b): return a
+	if (a<b):
+		low = a
+		high = b
+	else:
+		low = b
+		high = a
+	if (high%low == 0):
+		return low
+	if (divisor==0): divisor = (low/2)+1
+	if (a%divisor==0 and b%divisor==0):
+		return divisor
+	else: return gcf(a, b, divisor-1)
+	
+#Exercise 2
+#Write a function that returns prime numbers less than 121
+def primes_less_than(n):
+	primes = []
+	for num in range(n):
+		prime = True
+		for possible_prime in range(2,(num/2+1)):
+			if (num%possible_prime == 0):
+				prime = False
+				break;
+		if prime: primes.append(num)
+	return primes
+
+#Exercise 3
+#Write a function that gives a solution to Tower of Hanoi game
+#https://www.mathsisfun.com/games/towerofhanoi.html
+
+
+
